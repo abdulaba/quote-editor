071931e3aa8fa31b3ae243255949d5f26a9fb7ed
andres ibanez <andresibanezulloa@gmail.com>
2023-09-27 11:10:00 -0300

    quote controller - simple_form

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,13 +1,47 @@
 class QuotesController < ApplicationController
+  before_action :set_quotes, only: %i[show edit update destroy]
+
   def index
+    @quotes = Quote.all
   end
 
-  def show
-  end
+  def show; end
 
   def new
+    @quote = Quote.new
   end
 
-  def edit
+  def create
+    @quote = Quote.new(quote_params)
+    if @quote.save
+      redirect_to quotes_path, notice: "The quote has been created successfully"
+    else
+      render :new, status: :unprocessable_entity, alert: "An error ocurred, try again"
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @quote.update(quote_params)
+      redirect_to quotes_path, notice: "The quote has been updated successfully"
+    else
+      render :edit, status: :unprocessable_entity, alert: "An error ocurred, try again"
+    end
+  end
+
+  def destroy
+    @quote.destroy
+    redirect_to quotes_path, status: :see_other, notice: "The quote was deleted"
+  end
+
+  private
+
+  def quote_params
+    require.params(:quote).permit(:name)
+  end
+
+  def set_quote
+    @quote = Quote.find(params[:id])
   end
 end
