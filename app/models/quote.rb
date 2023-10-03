b723626d9dfb5062ae556668a0f1060e7736150a
andres ibanez <andresibanezulloa@gmail.com>
2023-10-03 19:52:44 -0300

    turbo stream redis

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,4 +1,8 @@
 class Quote < ApplicationRecord
   #Validations
-  # validates :name, presence: true
+  validates :name, presence: true
+
+  # after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quotes", locals: { quote: self }, target: "quotes" }
+  broadcasts_to ->(quotes) { "quotes" }, inserts_by: :prepend
+
 end
